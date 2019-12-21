59c0e0b229f7d3ee8ceda14279aa0afe413e04b8
Gerald Büchler <dev@genomast.de>
2019-12-21 12:06:14 +0100

    extend the pkg list and do some follow-up commands

--- usr/local/bin/install_my_packages.sh
+++ usr/local/bin/install_my_packages.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-pkglist="terminator nano htop vim iotop"
+pkglist="terminator nano htop vim iotop gimp nmap traceroute top apt-file pwgen openssh-client apache2-utils git git-sh"
 
 if [ "$(id -u)" -eq "0" ]; then
 	apt update
 	apt install -y ${pkglist}
+	apt-file update
 else
 	sudo apt update
 	sudo apt install -y ${pkglist}
+	sudo apt-file update
 fi
