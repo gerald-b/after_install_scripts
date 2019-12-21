353b0c34050473cb2bbd931c1d65ad43debcd4c9
Gerald Büchler <dev@genomast.de>
2019-12-21 09:50:09 +0100

    dont ask for install ...

--- usr/local/bin/install_my_packages.sh
+++ usr/local/bin/install_my_packages.sh
@@ -4,8 +4,8 @@ pkglist="terminator nano htop vim iotop"
 
 if [ "$(id -u)" -eq "0" ]; then
 	apt update
-	apt install ${pkglist}
+	apt install -y ${pkglist}
 else
 	sudo apt update
-	sudo apt install ${pkglist}
+	sudo apt install -y ${pkglist}
 fi
