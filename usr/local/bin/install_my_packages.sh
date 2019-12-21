31fedeab927dec705c509328eefed4c09529c2dc
Gerald Büchler <dev@genomast.de>
2019-12-21 09:45:09 +0100

    correct the packages script. (missing apt and quotes)

--- usr/local/bin/install_my_packages.sh
+++ usr/local/bin/install_my_packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-pkglist=terminator nano htop vim iotop
+pkglist="terminator nano htop vim iotop"
 
 if [ "$(id -u)" -eq "0" ]; then
 	apt update
 	apt install ${pkglist}
 else
 	sudo apt update
-	sudo install ${pkglist}
+	sudo apt install ${pkglist}
 fi
