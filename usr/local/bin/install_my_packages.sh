07f5ce1f8bf70683ab51b18809e5d9364d584377
Gerald Büchler <dev@genomast.de>
2019-12-22 21:31:40 +0100

    added vlc cutecom minicom gddrescue dia pkg's

--- usr/local/bin/install_my_packages.sh
+++ usr/local/bin/install_my_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkglist="terminator nano htop vim iotop gimp nmap traceroute procps apt-file pwgen openssh-client apache2-utils git git-sh exfat-utils exfat-fuse curl"
+pkglist="terminator nano htop vim iotop gimp nmap traceroute procps apt-file pwgen openssh-client apache2-utils git git-sh exfat-utils exfat-fuse curl vlc cutecom minicom gddrescue dia"
 
 if [ "$(id -u)" -eq "0" ]; then
 	apt update
